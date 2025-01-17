b3a6277edc2b07b47d47b9a2c2566d69d09a0f02
afk-sistems <ti.leidyinmob@gmail.com>
2025-01-16 21:47:42 -0500

    solucionado cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as path from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors();
   app.useStaticAssets(path.join(__dirname, '..', 'public'));
   app.setBaseViewsDir(path.join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
